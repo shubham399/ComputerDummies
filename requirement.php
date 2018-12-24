539d3e1b738b520b1ea78d45d8675ff7ac109d89
Shubham <shubham.399@gmail.com>
2018-12-24 17:32:22 +0530

    Update requirement.php

--- requirement.php
+++ requirement.php
@@ -1,17 +1,12 @@
 <?php
 function connect()
 {
-	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $server = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
-// echo $server;
-// 	echo $username;
-// 	echo $password;
-// 	echo $db;
-	$con=mysqli_connect($server, $username, $password, $db) or die("Failed to connect to MySQL: " . mysql_error());
+$con = new mysqli($server, $username, $password, $db);
 	return $con;
 }
 function get_qid($uid,$cid)
