1b0e6bb64ab8b6a344db628f294cdbfdddd31610
Shubham <shubham.399@gmail.com>
2018-12-26 15:03:28 +0530

    Update requirement.php

--- requirement.php
+++ requirement.php
@@ -815,9 +815,6 @@ $email->addContent(
 $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
 try {
     $response = $sendgrid->send($email);
-    print $response->statusCode() . "\n";
-    print_r($response->headers());
-    print $response->body() . "\n";
 } catch (Exception $e) {
     echo 'Caught exception: '. $e->getMessage() ."\n";
 }
